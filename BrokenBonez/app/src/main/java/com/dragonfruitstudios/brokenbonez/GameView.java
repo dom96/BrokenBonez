dom96/BrokenBonez

commit 6d1d127de883c366ec71ba2f00024f4e76ea2fa4

This is my old commit before broken touch classes are removed.

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/GameView.java
@@ -2,11 +2,8 @@ package com.dragonfruitstudios.brokenbonez;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -18,16 +15,18 @@ import android.view.View;
  *  you should call the complementary `unlockCanvas` method.
  */
 public class GameView extends View {
-    boolean ready;
-    Canvas canvas;
-    Paint paint;
+        boolean ready;
+        Canvas canvas;
+        Paint paint;
 
     public interface GVCallbacks {
-        void performDraw(GameView gameView);
-        void onSizeChanged(GameView gameView, int w, int h, int oldw, int oldh);
-    }
+    void performDraw(GameView gameView);
 
-    GVCallbacks callbacks;
+    void onSizeChanged(GameView gameView, int w, int h, int oldw, int oldh);
+
+}
+
+GVCallbacks callbacks;
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -65,6 +64,7 @@ public class GameView extends View {
 
     /**
      * Clear the GameView with the specified color.
+     *
      * @param color
      */
     public void clear(int color) {
@@ -75,6 +75,7 @@ public class GameView extends View {
 
     /**
      * Draw the specified text at the specified x,y coords with the specified color.
+     *
      * @param text
      * @param x
      * @param y

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/GameActivity.java
@@ -1,32 +1,23 @@
 package com.dragonfruitstudios.brokenbonez;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.PowerManager;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.WindowManager;
-import android.widget.Toast;
+import com.dragonfruitstudios.brokenbonez.Input.TouchHandler;
 
 /**
  * Game Activity class used for creating a new game view and game loop instance. Also defines some
  * device related features.
  */
 public class GameActivity extends Activity {
-
     private GameLoop gameLoop;
     GameView gameView;
-    
     PowerManager.WakeLock mWakeLock;
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         gameView = new GameView(this);
         // Power manager gives control over the power state of the android device.
@@ -45,43 +36,27 @@ public class GameActivity extends Activity {
     }
 
     @Override
-    public void onPause(){
+    public void onPause() {
         gameLoop.pause(); // Pauses gameLoop.
         super.onPause();
         this.mWakeLock.release(); // No need to lock anymore. Calling this saves device's battery.
     }
+
     @Override
-    public void onResume(){
+    public void onResume() {
         gameLoop.resume(); // Resumes gameLoop
         super.onResume();
         this.mWakeLock.acquire(); // Acquires the wake lock forcing the device to stay on.
     }
 
-    // Touch Handler
-    @Override
-    public boolean onTouchEvent(MotionEvent event){
-        int pointerIndex = event.getActionIndex();
-        int pointerId = event.getPointerId(pointerIndex);
-        int maskedAction = event.getActionMasked();
 
-        switch (maskedAction) {
-            case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_POINTER_DOWN: {
-                Log.d("FINGER TOUCHED SCREEN", "A finger has touched the screen!");
-                break;
-            }
-            case MotionEvent.ACTION_MOVE: {
-                Log.d("FINGER IS MOVING", "A finger is moving on the screen!");
-                break;
-            }
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_POINTER_UP:
-            case MotionEvent.ACTION_CANCEL: {
-                Log.d("FINGER WENT UP", "A finger has touched the screen and moved up!");
-                Log.d("ACTION EVENT CANCELLED", "Something else took control of the touch event!");
-                break;
-            }
-        }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        TouchHandler touchHandler = new TouchHandler(this, 222, 222);
+        touchHandler.onTouchDown(event);
+        touchHandler.onTouchOff(event);
         return true;
     }
 }
+
+

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Input/TouchHandler.java
@@ -1,38 +1,110 @@
 package com.dragonfruitstudios.brokenbonez.Input;
+
+import android.app.Activity;
+import android.graphics.PointF;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.MotionEvent;
 
-/**Handles touch, moving touch and multi-touch.
- * Could add some way to target the specific points on screen i.e. a certain area triggers a certain event.
- * UNCOMMENT @Override on use.
- */
-public class TouchHandler {
-    //@Override
-    public boolean onTouchEvent(MotionEvent event){
-        // get pointer index from the event object
-        int pointerIndex = event.getActionIndex();
-        // get pointer ID
-        int pointerId = event.getPointerId(pointerIndex);
-        // get masked (not specific to a pointer) action
-        int maskedAction = event.getActionMasked();
+/** Single & Multi-Touch input Handler.
+ *
+ *  Initialise the TouchHandler by passing in the activity and two target int counters
+ *
+ *  E.G.
+ *  TouchHandler touchHandler = new TouchHandler(this, 222, 222);
+ *
+ *  Then create a boolean method with a MotionEvent argument passed in.
+ *  And then execute methods from the API on your object within the method using your MotionEvent argument.
+ *  Don't forget to add onTouchOff as this will track when you take your finger off the screen and a silent
+ *  error will occur in the background if it isn't present.
+ *
+ *  E.G.
+ *  public boolean onTouchEvent(MotionEvent event){
+ *  touchHandler.onTouchDown(event);
+ *  touchHandler.onTouchOff(event);
+ *  }
+ *
+ *  TODO:
+ *  Multi-touch within this class does work, it has something to do with the action
+ *  pointer down & the action down. I will work on this as soon as I can feel free to
+ *  experiment with this code as there is still much more which can be added for greater overall
+ *  functionality. Check out this page for more info:
+ *  https://developer.android.com/reference/android/view/MotionEvent.html
+ *
+ * **/
 
-        // specify methods in relation to motion events
+public class TouchHandler {
+    float counterX;
+    float counterY;
+    public TouchHandler(Activity activity, int counterX, int counterY) {
+        this.counterX = counterX;
+        this.counterY = counterY;
+
+    }
+
+    public boolean onTouchDown(MotionEvent event) {
+        int pointerIndex = event.getActionIndex();
+        int pointerId = event.getPointerId(pointerIndex);
+        int maskedAction = event.getActionMasked();
+        SparseArray<PointF> mActivePointers;
         switch (maskedAction) {
-            /**ACTION_POINTER_DOWN used for multi-touch support.**/
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_POINTER_DOWN: {
-                //Do something
+                mActivePointers = new SparseArray<PointF>();
+                PointF f = new PointF();
+                f.x = event.getX(pointerIndex);
+                f.y = event.getY(pointerIndex);
+                mActivePointers.put(pointerId, f);
+                Log.d("FINGER TOUCHED SCREEN", "A finger has touched the screen!");
+                if (f.x < counterX && f.y > counterY) {
+                    Log.d("FINGER TOUCHED SCREEN", "FINGER TOUCHED SCREEN ON A SPECIFIC AREA");
+                }
                 break;
             }
+        }
+        return true;
+    }
+
+    public boolean onMove(MotionEvent event) {
+        int pointerIndex = event.getActionIndex();
+        int pointerId = event.getPointerId(pointerIndex);
+        int maskedAction = event.getActionMasked();
+        SparseArray<PointF> mActivePointers;
+        switch (maskedAction) {
             case MotionEvent.ACTION_MOVE: {
-                //Do something
+                mActivePointers = new SparseArray<PointF>();
+                PointF f = new PointF();
+                f.x = event.getX(pointerIndex);
+                f.y = event.getY(pointerIndex);
+                mActivePointers.put(pointerId, f);
+                Log.d("FINGER IS MOVING", "A finger is moving on the screen!");
+                if (f.x < counterX && f.y > counterY) {
+                    Log.d("FINGER MOVED", "FINGER MOVED ON A SPECIFIC AREA");
+                }
                 break;
             }
-            /**ACTION_POINTER_UP used for multi-touch support.
-               ACTION_CANCEL used if something else takes control of the touch event.**/
+        }
+        return true;
+    }
+
+    public boolean onTouchOff(MotionEvent event) {
+        int pointerIndex = event.getActionIndex();
+        int pointerId = event.getPointerId(pointerIndex);
+        int maskedAction = event.getActionMasked();
+        SparseArray<PointF> mActivePointers;
+        switch (maskedAction) {
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_POINTER_UP:
             case MotionEvent.ACTION_CANCEL: {
-                //Do something
+                mActivePointers = new SparseArray<PointF>();
+                PointF f = new PointF();
+                f.x = event.getX(pointerIndex);
+                f.y = event.getY(pointerIndex);
+                mActivePointers.put(pointerId, f);
+                Log.d("FINGER WENT OFF", "A finger has went off the screen!");
+                if (f.x < counterX && f.y > counterY) {
+                    Log.d("FINGER WENT OFF", "FINGER WENT OFF A SPECIFIC AREA");
+                }
                 break;
             }
         }

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/MenuActivity.java
@@ -1,33 +1,17 @@
 package com.dragonfruitstudios.brokenbonez;
-
         import android.app.Activity;
-        import android.content.Context;
         import android.content.Intent;
         import android.graphics.Bitmap;
-        import android.graphics.drawable.Icon;
-        import android.net.wifi.p2p.WifiP2pManager;
         import android.os.Bundle;
-        import android.os.PowerManager;
         import android.util.Log;
         import android.view.MotionEvent;
-        import android.view.View;
-        import android.view.Window;
         import android.view.WindowManager;
-        import android.widget.Button;
-
-        import android.app.Activity;
-        import android.os.Bundle;
         import android.view.Menu;
         import android.view.MenuInflater;
         import android.view.MenuItem;
-        import android.view.ViewGroup.LayoutParams;
         import android.widget.ImageView;
-        import android.widget.TextView;
         import android.widget.Toast;
-
         import com.dragonfruitstudios.brokenbonez.AssetLoading.AssetLoader;
-
-        import java.util.Timer;
 
 public class MenuActivity extends Activity {
     @Override
@@ -91,7 +75,6 @@ public class MenuActivity extends Activity {
         }
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {

commit 61bdb60045715c63d3bb18d9096d901a716cd9f1

Fix indentation broken by 6d1d127de883c3.

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/GameView.java
@@ -16,18 +16,18 @@ import android.view.View;
  *  you should call the complementary `unlockCanvas` method.
  */
 public class GameView extends View {
-        boolean ready;
-        Canvas canvas;
-        Paint paint;
+    boolean ready;
+    Canvas canvas;
+    Paint paint;
 
     public interface GVCallbacks {
-    void performDraw(GameView gameView);
+        void performDraw(GameView gameView);
 
-    void onSizeChanged(GameView gameView, int w, int h, int oldw, int oldh);
+        void onSizeChanged(GameView gameView, int w, int h, int oldw, int oldh);
 
-}
+    }
 
-GVCallbacks callbacks;
+    GVCallbacks callbacks;
 
     @Override
     protected void onDraw(Canvas canvas) {