109bcf3d5276cd23b63be3efa4cb9e36b6bf4b7b
gbaman <gbaman@users.noreply.github.com>
2016-02-28 00:45:44 +0000

    Add fixes to sound/music including volume control, ogg support, new play methods and new theme music

--- BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Gameplay/GameState.java
+++ BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Gameplay/GameState.java
@@ -22,7 +22,7 @@ public class GameState {
         // Load assets.
         this.assetLoader = assetLoader;
         this.assetLoader.AddAssets(new String[] {"bike/wheel_basic.png", "bike/body_one.png"});
-        this.assetLoader.AddAssets(new String[]{"bikeEngine.mp3", "bikeEngineRev.mp3"});
+        this.assetLoader.AddAssets(new String[]{"bikeEngine.mp3", "bikeEngineRev.mp3", "brokenboneztheme.ogg"});
 
         // Create a new physics simulator.
         this.physicsSimulator = new Simulator();
