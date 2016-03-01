dom96/BrokenBonez

commit ec1c1cb8ffb21b75437b2d0a34c44dc2b2cfb257

Removed empty Bike constructor & unused classes.

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Gameplay/GameLevel.java
@@ -155,7 +155,7 @@ public class GameLevel extends Level {
 
     }
 
-    public void update(float lastUpdate, VectorF bikePos) {
+    public void update(float lastUpdate) {
         this.bikePos = bikePos;
     }
 

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Gameplay/GameState.java
@@ -42,7 +42,7 @@ public class GameState {
     public void update(float lastUpdate) {
         bike.update(lastUpdate);
         physicsSimulator.update(lastUpdate);
-        currentLevel.update(lastUpdate, bike.getPos());
+        currentLevel.update(lastUpdate);
         camera.centerHorizontally(bike.getPos().x);
     }
 

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Game/Scenes/BikeSelectionScene.java
@@ -1,50 +1,0 @@
-package com.dragonfruitstudios.brokenbonez.Game.Scenes;
-
-import android.view.MotionEvent;
-
-import com.dragonfruitstudios.brokenbonez.AssetLoading.AssetLoader;
-import com.dragonfruitstudios.brokenbonez.Game.GameView;
-import com.dragonfruitstudios.brokenbonez.GameSceneManager;
-import com.dragonfruitstudios.brokenbonez.Menu.BikeSelectionState;
-import com.dragonfruitstudios.brokenbonez.Menu.MenuState;
-
-public class BikeSelectionScene extends Scene {
-    BikeSelectionState state;
-
-    public BikeSelectionScene(AssetLoader assetLoader, GameSceneManager gameSceneManager){
-        super(assetLoader, gameSceneManager);
-        this.state = new BikeSelectionState(assetLoader, gameSceneManager);
-    }
-
-    public void draw(GameView view) {
-        state.draw(view);
-    }
-
-    public void update(float lastUpdate) {
-        state.update(lastUpdate);
-    }
-
-    public void updateSize(int w, int h) {
-        state.updateSize(w, h);
-    }
-
-    public void onTouchEvent(MotionEvent event) {
-        state.onTouchEvent(event);
-    }
-
-    public void activate() {
-
-    }
-
-    public void deactivate() {
-
-    }
-
-    public void resume() {
-
-    }
-
-    public void pause() {
-
-    }
-}

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Game/Scenes/BikeShowcaseScene.java
@@ -1,48 +1,21 @@
 package com.dragonfruitstudios.brokenbonez.Game.Scenes;
 
 import android.view.MotionEvent;
-
 import com.dragonfruitstudios.brokenbonez.AssetLoading.AssetLoader;
 import com.dragonfruitstudios.brokenbonez.Game.GameView;
-import com.dragonfruitstudios.brokenbonez.Game.Level;
 import com.dragonfruitstudios.brokenbonez.GameSceneManager;
 import com.dragonfruitstudios.brokenbonez.Gameplay.Bike;
-import com.dragonfruitstudios.brokenbonez.Math.Collisions.Rect;
-import com.dragonfruitstudios.brokenbonez.Math.Physics.Simulator;
-import com.dragonfruitstudios.brokenbonez.Math.VectorF;
+import com.dragonfruitstudios.brokenbonez.Menu.BikeSelectionLevel;
+import com.dragonfruitstudios.brokenbonez.Menu.BikeSelectionState;
 
 public class BikeShowcaseScene extends Scene {
-    private class ShowcaseLevel extends Level {
-        Simulator sim;
-        AssetLoader loader;
-        public ShowcaseLevel(AssetLoader loader, Simulator sim) {
-            this.loader = loader;
-            this.sim = sim;
-
-            sim.createStaticBody(new Rect(new VectorF(100, 300), 800, 100));
-        }
-
-        @Override
-        public AssetLoader getAssetLoader() {
-            return loader;
-        }
-
-        @Override
-        public Simulator getPhysicsSimulator() {
-            return sim;
-        }
-
-        @Override
-        public VectorF getStartPoint() {
-            return new VectorF(400, 100);
-        }
-    }
-
-    ShowcaseLevel level;
+    BikeSelectionState state;
+    BikeSelectionLevel level;
     Bike bike;
     public BikeShowcaseScene(AssetLoader assetLoader, GameSceneManager gameSceneManager) {
         super(assetLoader, gameSceneManager);
-        level = new ShowcaseLevel(assetLoader, new Simulator());
+        state = new BikeSelectionState(assetLoader, gameSceneManager);
+        level = new BikeSelectionLevel(state);
         bike = new Bike(level, Bike.BodyType.Bike);
     }
 

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/GameLoop.java
@@ -7,7 +7,6 @@ import android.view.MotionEvent;
 
 import com.dragonfruitstudios.brokenbonez.AssetLoading.AssetLoader;
 import com.dragonfruitstudios.brokenbonez.Game.GameView;
-import com.dragonfruitstudios.brokenbonez.Game.Scenes.BikeSelectionScene;
 import com.dragonfruitstudios.brokenbonez.Game.Scenes.BikeShowcaseScene;
 import com.dragonfruitstudios.brokenbonez.Game.Scenes.GameScene;
 import com.dragonfruitstudios.brokenbonez.Game.Scenes.MenuScene;
@@ -46,10 +45,8 @@ public class GameLoop implements Runnable {
 
         MenuScene menuScene = new MenuScene(assetLoader, gameSceneManager);   //Create the new MenuScene
         GameScene gameScene = new GameScene(assetLoader, gameSceneManager);   //Create the new GameScene
-        BikeSelectionScene bikeSelectionScene = new BikeSelectionScene(assetLoader, gameSceneManager); //Create the BikeSelectionScene
         this.gameSceneManager.addScene("menuScene", menuScene, true);  //Add the MenuScene just created to the GameSceneManager, then sets it as the active scene
         this.gameSceneManager.addScene("gameScene", gameScene, false); //Add the Gamescene just created to the GameSceneManager, then makes sure it isn't set as active
-        this.gameSceneManager.addScene("bikeSelectionScene", bikeSelectionScene, false);
         this.gameSceneManager.addScene("bikeShowcaseScene",
                 new BikeShowcaseScene(assetLoader, gameSceneManager), false);
 

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Gameplay/Bike.java
@@ -37,11 +37,6 @@ public class Bike implements GameObject {
         Bike, Bicycle
     }
 
-    // TODO: Remove this empty constructor.
-    public Bike() {
-
-    }
-
     public Bike(Level currentLevel, BodyType bodyType) {
         this.currentLevel = currentLevel;
 

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Menu/BikeSelectionLevel.java
@@ -11,8 +11,9 @@ import com.dragonfruitstudios.brokenbonez.Math.Physics.Simulator;
 import com.dragonfruitstudios.brokenbonez.Math.VectorF;
 
 public class BikeSelectionLevel extends GameLevel {
-    BikeSelectionState bikeSelectionState; // Used to grab assets, and physics simulator.
-    VectorF startPoint; // Holds the coordinates which determine where the bike starts.
+
+    BikeSelectionState bikeSelectionState;
+    VectorF startPoint;
     VectorF bikePos;
 
     public BikeSelectionLevel(BikeSelectionState state) {
@@ -54,7 +55,7 @@ public class BikeSelectionLevel extends GameLevel {
         gameView.drawText(debugInfo, 100, 30, Color.WHITE);
     }
 
-    public void update(float lastUpdate, VectorF bikePos) {
+    public void update(float lastUpdate) {
         this.bikePos = bikePos;
     }
 

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Menu/BikeSelectionState.java
@@ -8,15 +8,12 @@ import com.dragonfruitstudios.brokenbonez.Game.Camera;
 import com.dragonfruitstudios.brokenbonez.Game.GameObject;
 import com.dragonfruitstudios.brokenbonez.Game.GameView;
 import com.dragonfruitstudios.brokenbonez.GameSceneManager;
-import com.dragonfruitstudios.brokenbonez.Gameplay.Bike;
 import com.dragonfruitstudios.brokenbonez.Gameplay.GameState;
 import com.dragonfruitstudios.brokenbonez.Math.Physics.Simulator;
 import com.dragonfruitstudios.brokenbonez.Math.VectorF;
 
 public class BikeSelectionState extends GameState implements GameObject {
     BikeSelectionLevel bikeSelectionLevel;
-    Bike firstBike;
-    SecondBike bikeTwo;
     private AssetLoader assetLoader;
     private GameSceneManager gameSceneManager;
     private Simulator physicsSimulator;
@@ -39,8 +36,6 @@ public class BikeSelectionState extends GameState implements GameObject {
         this.physicsSimulator = new Simulator();
         camera = new Camera(0, 0);
         bikeSelectionLevel = new BikeSelectionLevel(this);
-        firstBike = new Bike(bikeSelectionLevel, Bike.BodyType.Bicycle);
-        bikeTwo = new SecondBike(bikeSelectionLevel);
         this.assetLoader.AddAssets(new String[]{"nightsky.png", "tvnoise.png"});
         background = assetLoader.getBitmapByName("nightsky.png");
         scaledBackground = background.createScaledBitmap(background, getScreenWidth(), getScreenHeight(), false);
@@ -63,21 +58,13 @@ public class BikeSelectionState extends GameState implements GameObject {
 
     @Override
     public void update(float lastUpdate) {
-        firstBike.update(lastUpdate);
-        if(drawSecondBike() == true) {
-            bikeTwo.update(lastUpdate);
-        }
         physicsSimulator.update(lastUpdate);
-        bikeSelectionLevel.update(lastUpdate, firstBike.getPos());
+        bikeSelectionLevel.update(lastUpdate);
     }
 
     @Override
     public void updateSize(int w, int h) {
         bikeSelectionLevel.updateSize(w, h);
-        firstBike.updateSize(w, h);
-        if(drawSecondBike() == true) {
-            bikeTwo.updateSize(w, h);
-        }
         camera.updateSize(w, h);
     }
 
@@ -91,10 +78,6 @@ public class BikeSelectionState extends GameState implements GameObject {
         view.drawImage(select.scaledSelect, select.pos, select.rotation, GameView.ImageOrigin.TopLeft);
         view.setCamera(camera);
         bikeSelectionLevel.draw(view);
-        firstBike.draw(view);
-        if(drawSecondBike() == true){
-            bikeTwo.draw(view);
-        }
         physicsSimulator.draw(view);
     }
 

commit 9ea690164d757fa32d911d17a5182f39b24a149e

Fix scrolling bug introduced by ec1c1cb8ffb21b75437b2.
https://github.com/gbaman/dragonfruit-game/commit/ec1c1cb8ffb21b75437b2d0a34c44dc2b2cfb257

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Gameplay/GameLevel.java
@@ -155,7 +155,7 @@ public class GameLevel extends Level {
 
     }
 
-    public void update(float lastUpdate) {
+    public void update(float lastUpdate, VectorF bikePos) {
         this.bikePos = bikePos;
     }
 

BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Gameplay/GameState.java
@@ -48,7 +48,7 @@ public class GameState {
     public void update(float lastUpdate) {
         bike.update(lastUpdate);
         physicsSimulator.update(lastUpdate);
-        currentLevel.update(lastUpdate);
+        currentLevel.update(lastUpdate, bike.getPos());
         camera.centerHorizontally(bike.getPos().x);
     }
 