d62709b92a5d42cfe6ede1bf989edd606aae7f52
Dominik Picheta <dominikpicheta@gmail.com>
2016-03-09 17:50:15 +0000

    Fix compilation of LevelSelectionLevel.

--- BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Menu/LevelSelectionLevel.java
+++ BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Menu/LevelSelectionLevel.java
@@ -3,10 +3,12 @@ package com.dragonfruitstudios.brokenbonez.Menu;
 import android.content.res.Resources;
 import com.dragonfruitstudios.brokenbonez.AssetLoading.AssetLoader;
 import com.dragonfruitstudios.brokenbonez.Game.GameView;
+import com.dragonfruitstudios.brokenbonez.Game.Level;
 import com.dragonfruitstudios.brokenbonez.Gameplay.GameLevel;
 import com.dragonfruitstudios.brokenbonez.Math.Physics.Simulator;
+import com.dragonfruitstudios.brokenbonez.Math.VectorF;
 
-public class LevelSelectionLevel extends GameLevel {
+public class LevelSelectionLevel extends Level {
     LevelSelectionState levelSelectionState;
 
     public LevelSelectionLevel(LevelSelectionState state){
@@ -24,6 +26,11 @@ public class LevelSelectionLevel extends GameLevel {
 
     public void update(float lastUpdate) {
 
+    }
+
+    @Override
+    public VectorF getStartPoint() {
+        return new VectorF(0, 0);
     }
 
     public AssetLoader getAssetLoader() {
