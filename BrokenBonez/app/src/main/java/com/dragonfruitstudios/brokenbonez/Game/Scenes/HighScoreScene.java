116504dc5556aa501270ba01fafb4819e00512ce
nmcparland <nmcparland@outlook.com>
2016-03-16 16:34:11 +0000

    high scores page fix

--- BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Game/Scenes/HighScoreScene.java
+++ BrokenBonez/app/src/main/java/com/dragonfruitstudios/brokenbonez/Game/Scenes/HighScoreScene.java
@@ -8,19 +8,22 @@ import com.dragonfruitstudios.brokenbonez.Game.GameView;
 import com.dragonfruitstudios.brokenbonez.GameSceneManager;
 import com.dragonfruitstudios.brokenbonez.HighScores.HighScore;
 import com.dragonfruitstudios.brokenbonez.HighScores.HighScoreData;
+import com.dragonfruitstudios.brokenbonez.Menu.ImageButton;
+
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class HighScoreScene extends Scene {
     private GameView gameView;
     protected ArrayList<HighScoreData> currentHighScores = new ArrayList<HighScoreData>();
     private HighScore highScore;
+    private ImageButton clearHighScore;
 
     public HighScoreScene(AssetLoader assetLoader, GameSceneManager gameSceneManager){
         super(assetLoader, gameSceneManager);
         this.gameView = gameSceneManager.gameView;
         this.highScore = new HighScore(this.gameView);
+        clearHighScore = new ImageButton("clear.png", assetLoader, 120, 840, 340, 76);
     }
 
     public void updateCurrentHighScoresList(){
@@ -38,7 +41,10 @@ public class HighScoreScene extends Scene {
 
     @Override
     public void onTouchEvent(MotionEvent event) {
-
+        clearHighScore.onTouchEvent(event);
+        if(clearHighScore.onTouchEvent(event) == true){
+                currentHighScores.clear();
+        }
     }
 
     @Override
@@ -74,14 +80,14 @@ public class HighScoreScene extends Scene {
 
     @Override
     public void draw(GameView view) {
-
+        clearHighScore.draw(view);
         HighScoreData highScore;
-        gameView.drawText("High scores", (getScreenWidth() / 2) -70, 160, Color.WHITE, 40);
+        gameView.drawText("High Scores", getScreenWidth() / 2, 100, Color.WHITE, 66);
 
         for (int i = this.currentHighScores.size() -1; i >= 0; i--) {
             highScore = currentHighScores.get(i);
-            gameView.drawText(highScore.name + "-" + highScore.dateString + " - " + highScore.time + " " + highScore.score, (getScreenWidth() / 2) - 70, (200 + (i * 40)), Color.WHITE, 40);
-
+            gameView.drawText("Name:" + "                     " + "Time(Sec):" + "                        " + "Score:", getScreenWidth() / 2, 200, Color.WHITE, 66);
+            gameView.drawText("                                                      " + highScore.name + "                         " + highScore.time/1000 + "                              " + highScore.score, getScreenWidth() / 4, (300 + (i * 40)), Color.WHITE, 66);
         }
     }
 }
